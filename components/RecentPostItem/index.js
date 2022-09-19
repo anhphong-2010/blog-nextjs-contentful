70c7c1232d3680661c4d3cb486db6b85dec73544
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-09-19 18:18:10 +0700

    hover animation

--- components/RecentPostItem/index.js
+++ components/RecentPostItem/index.js
@@ -10,43 +10,46 @@ import _ from "lodash";
 export default function RecentPostItem(props) {
   const { post } = props;
   return (
-    <article
-      className="p-4 rounded-lg bg-white dark:bg-gray-900 flex flex-col space-y-2 mb-3 sm:mb-6 "
-      key={post.sys.id}
-    >
-      <div
-        className="relative bg-slate-100 rounded-lg overflow-hidden border dark:border-white border-gray-300"
-        style={{ width: "100%", height: 200 }}
+    <Link className="cursor-pointer" href={`/blog/${post.slug}`} passHref>
+      <article
+        className="group rounded-lg bg-white dark:bg-gray-900 flex flex-col space-y-2 mb-3 sm:mb-6 border border-white dark:border-gray-900"
+        key={post.sys.id}
       >
-        <Image
-          src={postFormatter.thumbnail(post)}
-          alt={post.title}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
-
-      <div className="flex flex-col space-y-2">
-        <Link className="cursor-pointer" href={`/blog/${post.slug}`} passHref>
-          <h2 className="text-gray-900 dark:text-white text-3xl font-medium py-4 cursor-pointer">
-            {post.title}
-          </h2>
-        </Link>
-        <div>
-          <PublishedDate
-            classStr="text-gray-900 dark:text-white"
-            date={post.date}
-          />
-        </div>
         <div
-          className={`text-gray-900 dark:text-white text-sm ${ContentListStyles.contentList__excerpt}`}
+          className="cursor-pointer relative bg-slate-100 rounded-tr-lg rounded-tl-lg overflow-hidden"
+          style={{ width: "100%", height: 200 }}
         >
-          <ReactMarkdown
-            children={post.description}
-            components={ReactMarkdownRenderers(post.description)}
+          <Image
+            src={postFormatter.thumbnail(post)}
+            alt={post.title}
+            layout="fill"
+            objectFit="cover"
+            className="group-hover:scale-110 duration-700"
           />
         </div>
-      </div>
-    </article>
+
+        <div className="px-4 flex flex-col space-y-2">
+          <Link className="cursor-pointer" href={`/blog/${post.slug}`} passHref>
+            <h2 className="text-gray-900 dark:text-white text-3xl font-medium py-4 cursor-pointer">
+              {post.title}
+            </h2>
+          </Link>
+          <div>
+            <PublishedDate
+              classStr="text-gray-900 dark:text-white"
+              date={post.date}
+            />
+          </div>
+          <div
+            className={`text-gray-900 dark:text-white text-sm ${ContentListStyles.contentList__excerpt}`}
+          >
+            <ReactMarkdown
+              children={post.description}
+              components={ReactMarkdownRenderers(post.description)}
+            />
+          </div>
+        </div>
+      </article>
+    </Link>
   );
 }
