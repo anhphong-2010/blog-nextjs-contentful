2284b4607bc556ec2cc7a25ffb201dcb8d362487
anhphong-2010 <anhphongnw.2010@gmail.com>
2023-03-10 17:04:13 +0700

    a tag for relative post item

--- components/RelatePostList/index.js
+++ components/RelatePostList/index.js
@@ -5,43 +5,45 @@ import Author from "@components/Author";
 import ReactMarkdown from "react-markdown";
 import ReactMarkdownRenderers from "@utils/react-mark-down-renderers";
 
-export default function RelatePostList(props) {
-  const { posts, post } = props;
-
+export default function RelatePostList({ posts, post }) {
   return (
-    <div className="mb-12 lg:my-12">
-      <div className="mb-4 p-4 rounded-lg bg-gray-800 dark:bg-white">
-        <div className={`py-6 flex-col flex items-start space-y-2`}>
-          <Author
-            size={60}
-            data={post}
-            textClassName="text-white dark:text-black"
-          />
-          <div className={`dark:text-gray-900 text-white text-sm break-all flex flex-col space-y-2`}>
-            <ReactMarkdown
-              children={_.get(post, "author.description")}
-              components={ReactMarkdownRenderers(
-                _.get(post, "author.description")
-              )}
+    <div>
+      <div className="mb-12 lg:my-12">
+        <div className="mb-4 p-4 rounded-lg bg-gray-800 dark:bg-white">
+          <div className={`py-6 flex-col flex items-start space-y-2`}>
+            <Author
+              size={60}
+              data={post}
+              textClassName="text-white dark:text-black"
             />
+            <div
+              className={`dark:text-gray-900 text-white text-sm break-all flex flex-col space-y-2`}
+            >
+              <ReactMarkdown
+                children={_.get(post, "author.description")}
+                components={ReactMarkdownRenderers(
+                  _.get(post, "author.description")
+                )}
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="p-4 py-6 rounded-lg bg-gray-800 dark:bg-white">
-        <h2 className="text-2xl font-semibold text-white dark:text-gray-900 mb-6">
-          Others post from this author
-        </h2>
-        <div className="flex space-y-4 flex-col">
-          {_.map(posts, (post, index) => (
-            <React.Fragment key={index}>
-              <Link href={`/blog/${post.slug}`} passHref>
-                <div className="cursor-pointer text-white dark:text-gray-900">
-                  {_.get(post, "title")}
-                </div>
-              </Link>
-            </React.Fragment>
-          ))}
+        <div className="p-4 py-6 rounded-lg bg-gray-800 dark:bg-white">
+          <h2 className="text-2xl font-semibold text-white dark:text-gray-900 mb-6">
+            Others post from this author
+          </h2>
+          <div className="flex space-y-4 flex-col">
+            {_.map(posts, (post_item, index) => (
+              <div key={index}>
+                <a href={`/blog/${_.get(post_item, "slug", "")}`}>
+                  <div className="cursor-pointer text-white dark:text-gray-900">
+                    {_.get(post_item, "title", "")}
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- layouts/LayoutSideBar.js
+++ layouts/LayoutSideBar.js
@@ -4,15 +4,13 @@ import _ from "lodash";
 
 export default function LayoutSideBar({ children }) {
   return (
-    <div>
-      <div className="container mx-4 sm:mx-auto">
-        <Header />
-        <div className="flex flex-col lg:flex-row lg:space-x-3 w-full">
-          <div>
-            <Layout name="side-bar">{_.get(children, "0", {})}</Layout>
-          </div>
-          <Layout name="content">{_.get(children, "1", {})}</Layout>
+    <div className="container mx-4 sm:mx-auto">
+      <Header />
+      <div className="flex flex-col lg:flex-row lg:space-x-3 w-full">
+        <div>
+          <Layout name="side-bar">{_.get(children, "0", {})}</Layout>
         </div>
+        <Layout name="content">{_.get(children, "1", {})}</Layout>
       </div>
     </div>
   );
